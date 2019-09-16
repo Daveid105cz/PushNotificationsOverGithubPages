3c74890d6cf0d6b8e985ad2c0773d1dd3f1dce65
Daveid105cz <davi.podeszwa@gmail.com>
2019-09-16 15:30:00 +0200

    Fixed URL

--- mainCode.js
+++ mainCode.js
@@ -50,7 +50,7 @@ function subscribeForPush()
 		postData.append("Auth",base64Encode(subscription.getKey("auth")));
 		postData.append("Password","secretText");
 
-        fetch("//podeszwa.8u.cz/PushTest/saveSubscription.php", {
+        fetch("http://podeszwa.8u.cz/PushTest/saveSubscription.php", {
           method: "POST",
           body: postData
         }).then(res => {
